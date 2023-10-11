5e85fa6aaf6108bbab50e584fe5a4b04ca8c854a
mossy boy <alexshu1812@gmail.com>
2023-10-11 18:03:26 +0800

    PRL new1

---
+++ (lvl 2) Classes & Objects & Methods/built-in classes & methods.py
@@ -0,0 +1,14 @@
+a = 'string'
+b = 42
+c = 4.2
+d = True
+
+print(
+    len(a), a.__len__(),
+
+    b + 8, b.__add__(8),
+    b - 2, b.__sub__(2),
+
+    str(c), c.__str__(),
+    int(c), c.__int__()
+)
--- (path)/test.py
+++
@@ -1,7 +0,0 @@
-class Car:
-    def turn_on(self):
-        return self
-    
-car =Car()
-
-print(car.turn_on())
